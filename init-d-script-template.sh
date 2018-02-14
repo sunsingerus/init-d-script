a97602393e7512c036808efb28c840926535c7e1
Vladislav Klimenko <sunsingerus@gmail.com>
2018-02-14 17:32:36 +0300

    init.d script

---
+++ init-d-script-template.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          <NAME>
+# Required-Start:    $local_fs $network $named $time $syslog
+# Required-Stop:     $local_fs $network $named $time $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: <SHORT DESCRIPTION>
+# Description:       <DESCRIPTION>
+### END INIT INFO
+
+
+SERVICE_NAME=$(basename $0)
+RUN_CMD=
+IS_CMD_CAPABLE="yes"
+PID_FILE=/var/run/${SERVICE_NAME}.pid
+LOG_FILE=/var/log/${SERVICE_NAME}.log
+RUN_AS_USER=<USERNAME>
+WORK_DIR=/
+SOFT_STOP_SIGNAL=SIGINT
+HARD_STOP_SIGNAL=SIGKILL
+
+function get_pid()
+{
+	[ -f "$PID_FILE" ] && cat "$PID_FILE"
+}
+
+function delete_pid_file()
+{
+	if [ -f "$PID_FILE" ]; then
+		rm "$PID_FILE"
+	fi
+}
+
+function is_running()
+{
+	kill -0 $(get_pid)
+}
+
+function start_capable_service()
+{
+	sudo -u $RUN_AS_USER "$RUN_CMD"
+}
+
+function start_incapable_service()
+{
+	local CMD="${RUN_CMD} &> \"$LOG_FILE\" & echo \$!"
+	sudo -u $RUN_AS_USER "$CMD" > "$PID_FILE"
+}
+
+function start()
+{
+	if is_running; then
+		echo -n "$SERVICE_NAME already running as: "
+		ps -p $(get_pid)
+		echo
+		exit 1
+	fi
+
+	echo "Starting $SERVICE_NAME"
+	cd $WORK_DIR
+
+	if [ $IS_CMD_CAPABLE == "yes" ] then;
+		start_capable_service
+	else
+		start_incapable_service
+	fi
+
+	cd -
+
+	if ! is_running; then
+		echo "ERROR: unable to start $SERVICE_NAME"
+		echo "Check log file(s)"
+		echo $LOG_FILE
+		exit 1
+	fi
+}
+
+function stop()
+{
+	STOP_SIGNAL=$1
+
+	if ! is_running; then
+		echo "$SERVICE_NAME is not running"
+		exit 1
+	fi
+
+	echo "Send ${STOP_SIGNAL} to $SERVICE_NAME"
+	kill -${STOP_SIGNAL} $(get_pid)
+
+	echo "Wait for $SERVICE_NAME to exit"
+	while is_running; do
+		echo -n '.'
+		sleep 1
+	done
+	echo "$SERVICE_NAME is not running"
+
+	delete_pid_file
+}
+
+function stop_soft()
+{
+	stop $SOFT_STOP_SIGNAL
+}
+
+function stop_hard()
+{
+	stop $HARD_STOP_SIGNAL
+}
+
+function restart()
+{
+	stop
+	start
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop_soft
+		;;
+	stop_hard)
+		stop_hard
+		;;
+	retart)
+		restart
+		;;
+	*)
+		echo "Usage: $0 {start|stop|stop_hard|restart}"
+esac
+
